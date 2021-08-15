3f64ae7b455a99a675db3018ccd68af88cdf2877
ahmad-musa <ahmadmusa0987@gmail.com>
2021-08-15 23:51:41 +0600

    admin panel: category add activated

---
+++ classes/Category.php
@@ -0,0 +1,42 @@
+<?php
+    include_once '../lib/Database.php';
+    include_once '../helpers/Format.php';
+?>
+
+
+<?php
+
+    class Category{
+        private $db;
+        private $fm;
+    
+        public function __construct(){
+            $this->db = new Database();
+            $this->fm = new Format();
+        }
+
+        public function catInsert($catName){
+            $catName = $this->fm->validation($catName);
+            
+            $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+            if (empty($catName)){
+                $msg = "<span class = 'error'> Category field must not be empty! </span>";
+                return $msg;
+            } else {
+                $query = "INSERT INTO tbl_category(catName) VALUES('$catName') ";
+                $catinsert = $this->db->insert($query);
+                if ($catinsert) {
+                    $msg= "<span class = 'success'> Category inserted successfully. </span>";
+                    return $msg;
+                } else {
+                    $msg= "<span class = 'error'> Category not inserted. </span>";
+                    return $msg;
+                }
+            }
+
+        }
+
+    }
+
+?>
