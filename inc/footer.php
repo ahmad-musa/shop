f6c4553123a2cc303b55016d5f7ea9a0e15fa202
ahmad-musa <ahmadmusa0987@gmail.com>
2022-07-30 01:24:54 +0600

    Footer minor change

--- inc/footer.php
+++ inc/footer.php
@@ -6,28 +6,28 @@
 				<div class="col_1_of_4 span_1_of_4">
 						<h4>Information</h4>
 						<ul>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Customer Service</a></li>
-						<li><a href="#"><span>Advanced Search</span></a></li>
-						<li><a href="#">Orders and Returns</a></li>
-						<li><a href="#"><span>Contact Us</span></a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Customer Service</a></li>
+							<li><a href="#"><span>Advanced Search</span></a></li>
+							<li><a href="#">Orders and Returns</a></li>
+							<li><a href="#"><span>Contact Us</span></a></li>
 						</ul>
 					</div>
 				<div class="col_1_of_4 span_1_of_4">
 					<h4>Why buy from us</h4>
 						<ul>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Customer Service</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="#"><span>Site Map</span></a></li>
-						<li><a href="#"><span>Search Terms</span></a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Customer Service</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+							<li><a href="#"><span>Site Map</span></a></li>
+							<li><a href="#"><span>Search Terms</span></a></li>
 						</ul>
 				</div>
 				<div class="col_1_of_4 span_1_of_4">
-					<h4>My account</h4>
+					<h4>My Account</h4>
 						<ul>
 							<li><a href="#">Sign In</a></li>
-							<li><a href="index.php">View Cart</a></li>
+							<li><a href="cart.php">View Cart</a></li>
 							<li><a href="#">My Wishlist</a></li>
 							<li><a href="#">Track My Order</a></li>
 							<li><a href="#">Help</a></li>
@@ -36,7 +36,6 @@
 				<div class="col_1_of_4 span_1_of_4">
 					<h4>Contact</h4>
 						<ul>
-							<li><span>+88-01957323872</span></li>
 							<li><span>+88-01957323872</span></li>
 						</ul>
 						<div class="social-icons">
@@ -52,7 +51,7 @@
 				</div>
 			</div>
 			<div class="copy_right">
-				<p> <a href="http://github.com/ahmad-musa" target= "_blank" >Pushtalk</a> &copy; All rights Reseverd </p>
+				<p> <a href="http://github.com/ahmad-musa/shop" target= "_blank">Pushtalk</a> &copy; All Rights Reserved </p>
 		   </div>
      </div>
     </div>
@@ -77,7 +76,6 @@
     <a href="#" id="toTop" style="display: block;"> 
 		<span id="toTopHover" style="opacity: 1;"> 
 		</span> 
-		
 	</a>
     
 	<link href="css/flexslider.css" rel='stylesheet' type='text/css' />
