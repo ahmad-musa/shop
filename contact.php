e31b9b055078b1840b08860ee10019ef4dcf522d
Ahmad Musa <ahmadmusa0987@gmail.com>
2023-07-23 00:29:47 +0600

    Contact form: validation & styling

--- contact.php
+++ contact.php
@@ -1,12 +1,44 @@
 <?php include 'inc/header.php';?>
 
+<?php 
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+		$name     = $fm->validation($_POST['name']);
+        $name     = mysqli_real_escape_string($db->link, $name);
+		
+		$email     = $fm->validation($_POST['email']);
+        $email     = mysqli_real_escape_string($db->link, $email);
+
+		$subject     = $fm->validation($_POST['subject']);
+        $subject     = mysqli_real_escape_string($db->link, $subject);
+
+		$body     = $fm->validation($_POST['body']);
+        $body     = mysqli_real_escape_string($db->link, $body);
+
+
+		$error 	= " ";
+		if(empty($name)){
+			$error 	= "Name must not be empty! ";
+		}elseif(empty($email)){
+			$error 	= "Email must not be empty! ";
+		}elseif(empty($body)){
+			$error 	= "Message must not be empty! ";
+		}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			$error 	= "Invalid Email!";
+		}else{
+			$msg = "Message sent successfully!";
+		}
+
+		}
+?>
+
  <div class="main">
     <div class="content">
     	<div class="support">
   			<div class="support_desc">
   				<h3>Live Support</h3>
   				<p><span>24 hours | 7 days a week | 365 days a year &nbsp;&nbsp; Live Technical Support</span></p>
-  				<p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+  				<p> Please let us know about your thoughts. Feel free to knock us.</p>
   			</div>
   				<img src="web/images/contact.png" alt="" />
   			<div class="clear"></div>
@@ -15,25 +47,32 @@
 				<div class="col span_2_of_3">
 				  <div class="contact-form">
 				  	<h2>Contact Us</h2>
-					    <form>
+					<?php
+						if(isset($error)){ 		echo "<span class='error'> $error </span> ";
+						}
+						if(isset($msg)){ 		echo "<span class='success'> $msg </span> ";
+						}
+					?>
+
+					    <form action=" " method="post">
 					    	<div>
 						    	<span><label>NAME</label></span>
-						    	<span><input type="text" value=""></span>
+						    	<span><input type="text" name="name" class="form_input"></span>
 						    </div>
 						    <div>
 						    	<span><label>E-MAIL</label></span>
-						    	<span><input type="text" value=""></span>
+						    	<span><input type="email" name="email" class="form_input"></span>
 						    </div>
 						    <div>
-						     	<span><label>MOBILE.NO</label></span>
-						    	<span><input type="text" value=""></span>
+						     	<span><label>SUBJECT</label></span>
+						    	<span><input type="text" name="subject"class="form_input"></span>
 						    </div>
 						    <div>
-						    	<span><label>SUBJECT</label></span>
-						    	<span><textarea> </textarea></span>
+						    	<span><label>MESSAGE</label></span>
+						    	<span><textarea name="body" class="form_input"> </textarea></span>
 						    </div>
-						   <div>
-						   		<span><input type="submit" value="SUBMIT"></span>
+						   <div class="center">
+						   		<span><input type="submit" value="SEND" class="btn btn_send"></span>
 						  </div>
 					    </form>
 				  </div>
